7f3fa357a3563b5e527909275b06b3a7a20c190a
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-11-06 20:23:27 +0530

    Update image source path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" style={{backgroundColor:'#1a1a1a'}}>
           <Toolbar>
-            <Link to={'/'}><img src="/public/employeea-white.png" alt="employeea-logo" style={{ width: 150, height: Autocomplete, marginLeft: '1.5rem' }}/></Link>
+            <Link to={'/'}><img src="/employeea-white.png" alt="employeea-logo" style={{ width: 150, height: Autocomplete, marginLeft: '1.5rem' }}/></Link>
             <Box sx={{ ml: 'auto' }}>
               <Button sx={{fontSize: '1.3rem', fontFamily: "Oswald", '& .MuiTouchRipple-root': {color: 'white'}}}><Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}>Home</Link></Button>
               <Button sx={{fontSize: '1.3rem', fontFamily: "Oswald", '& .MuiTouchRipple-root': {color: 'white'}, marginLeft: '2em' }}><Link to={'/form'} style={{ textDecoration: 'none', color: 'white' }}>Employee Form</Link></Button>
